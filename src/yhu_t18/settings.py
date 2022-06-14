169d648573c4aa585f93f787bdcd156792de564a
makinzm <yuruseba@yahoo.co.jp>
2022-06-14 15:01:45 +0900

    settings urls

--- src/yhu_t18/settings.py
+++ src/yhu_t18/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'yhu_t18.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "htmls"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
