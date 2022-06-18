f70695a98f14604889996f22d4b97022225ed860
hisashi-y <yoshizawa.hisashi.yi6@is.naist.jp>
2022-06-18 07:43:01 +0000

    test.py内の時間計測のための変数名を変更

--- src/PDFtoBow/test.py
+++ src/PDFtoBow/test.py
@@ -6,18 +6,19 @@ file_dir1 = 'sample1.pdf' # Size of sample1: 441 words
 file_dir2 = 'sample2.pdf' # Size of sample2: 552 words
 
 # 処理時間を計測
-s = time()
+s1 = time()
 BoW1 = get_BoW(file_dir1)
-g = time()
+g1 = time()
 print('Test result on sample1:')
-print(f'Processing time: {g-s:.4f} [s]')
+print(f'Processing time: {g1-s1:.4f} [s]')
 
-s = time()
+s2 = time()
 BoW2 = get_BoW(file_dir2)
-g = time()
+g2 = time()
 print('Test result on sample2:')
-print(f'Processing time: {g-s:.4f} [s]')
+print(f'Processing time: {g2-s2:.4f} [s]')
 
+# 取得したBoW(dict)の内容をチェックするためにjsonファイルとして出力
 # with open('test_BoW1.json', 'w') as f:
 #     json.dump(BoW1, f, indent = 4)
 
