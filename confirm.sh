11add217a507b0171218a7d349cdce48579d2c86
makinzm <yuruseba@yahoo.co.jp>
2022-06-26 11:02:00 +0900

    trying

---
+++ confirm.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# docker compose up -d
+# docker compose exec db2 /bin/bash
+# psql -h localhost -U db_user -d postgres
+# ^D
+# cd docker-entrypoint-initdb.d/
+# head -n5 word_meaning.sql 
+# \dt public.* で確認をしても myTableが追加されていない
--- src/words/models.py
+++ src/words/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+class Meaning(models.Model):
+    """
+    単語の意味と単語を持ち合わせている.
+    事前にsrcの中に保存されている
+
+    Args:
+        models (_type_): _description_
+    """
+    word = models.CharField(max_length=100)
+    meaning = models.CharField(max_length=200)
+
 # Create your models here.
 class Word(models.Model):
     """ Wordのclass
@@ -17,11 +28,12 @@ class Word(models.Model):
     thesis_id = models.IntegerField(blank=True, null=True) # 本当は自動補完したい
     pronounce_id = models.IntegerField(blank=True, null=True)
     phonetics_id = models.IntegerField(blank=True, null=True)
-    meaning_id = models.IntegerField(blank=True, null=True)
+    
+    meaning_id = models.ForeignKey(Meaning, null=True, on_delete=models.SET_NULL, related_name='id')
+    default_meaning = models.ForeignKey(Meaning, null=True, on_delete=models.SET_NULL, related_name='meaning')
 
     def __str__(self):
         return self.word
     
     class Meta:
         ordering = ["-importance"]
-
