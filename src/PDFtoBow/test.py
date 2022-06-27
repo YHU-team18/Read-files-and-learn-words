844e298a1bda1d783a46f908b9e9ef4cb1cd8262
hisashi-y <yoshizawa.hisashi.yi6@is.naist.jp>
2022-06-27 05:24:31 +0000

    誤ったコメントを訂正

--- src/PDFtoBow/test.py
+++ src/PDFtoBow/test.py
@@ -11,12 +11,14 @@ file_dir2 = os.path.normpath(os.path.join(base_path, '../PDFtoBoW/sample2.pdf'))
 
 # 処理時間を計測
 s1 = time()
+# sample1.pdfの単語頻度、単語の意味のdictをそれぞれ取得
 BoW_frequency1, BoW_meaning1 = get_BoW(file_dir1)
 g1 = time()
 print('Test result on sample1:')
 print(f'Processing time: {g1-s1:.4f} [s]')
 
 s2 = time()
+# sample2.pdfの単語頻度、単語の意味のdictをそれぞれ取得
 BoW_frequency2, BoW_meaning2 = get_BoW(file_dir2)
 g2 = time()
 print('Test result on sample2:')
@@ -24,18 +26,19 @@ print(f'Processing time: {g2-s2:.4f} [s]')
 
 # 取得したBoW(dict)の内容をチェックするためにjsonファイルとして出力
 # sample1.pdfの{'単語': 頻度}のdictを保存
-# with open('test_BoW1_frequency.json', 'w') as f:
+# with open('sample1_frequency.json', 'w') as f:
 #     json.dump(BoW_frequency1, f, indent = 4)
-# sample1.pdfの{'単語': '辞書のエントリー'}のdictを保存
-# with open('test_BoW2_frequency.json', 'w') as f:
+# sample2.pdfの{'単語': 頻度}のdictを保存
+# with open('sample2_frequency.json', 'w') as f:
 #     json.dump(BoW_frequency2, f, indent = 4)
 
-# sample2.pdfの{'単語': 頻度}のdictを保存
-# with open('test_BoW1_meaning.json', 'w', encoding = 'utf-8') as f:
+# sample1.pdfの{'単語'; '辞書のエントリー'}のdictを保存
+# with open('sample1_meaning.json', 'w', encoding = 'utf-8') as f:
 #     json.dump(BoW_meaning1, f, indent = 4)
-# sample2.pdfの{'単語': '辞書のエントリー'}のdictを保存
-# with open('test_BoW2_meaning.json', 'w', encoding = 'utf-8') as f:
+# sample2.pdfの{'単語'; '辞書のエントリー'}のdictを保存
+# with open('sample2_meaning.json', 'w', encoding = 'utf-8') as f:
 #     json.dump(BoW_meaning2, f, indent = 4)
 
 # {'単語': '辞書のエントリー'}のBoWについてちゃんと対応できているかテスト
 print(BoW_meaning1['activity'])
+print(BoW_meaning2['attributive'])
