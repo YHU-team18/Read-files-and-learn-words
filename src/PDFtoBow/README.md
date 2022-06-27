698cb1a0be101e03f09fd4b019f7895649adcb19
hisashi-y <yoshizawa.hisashi.yi6@is.naist.jp>
2022-06-27 11:00:02 +0000

    英和辞典のリポジトリをフォークしてデータ処理するように変更

--- src/PDFtoBow/README.md
+++ src/PDFtoBow/README.md
@@ -8,6 +8,5 @@ The original txt file is mofified and stored as 'words_by_frequency.txt'.
 # About English-Japanese dictionary data used here
 Public Domain / CC0
 
-The English-Japanese dictionary data used here is taken from the repository by kujirahand (https://github.com/kujirahand/EJDict).
-The original data is stored as 'ejdict-hand-utf8.txt'
+The English-Japanese dictionary data used here is taken from the repository by kujirahand (https://github.com/kujirahand/EJDict). The repository is forked (https://github.com/hisashi-y/EJDict). The original data consists of 26 files. These files are merged into one json file and  stored as 'ejdict_all.json'
 
--- src/PDFtoBow/test.py
+++ src/PDFtoBow/test.py
@@ -2,6 +2,7 @@ import os
 from time import time
 import json
 from PDFtoBoW import get_BoW
+from PDFtoBoW import get_meaning
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 file_dir1 = os.path.normpath(os.path.join(base_path, '../PDFtoBoW/sample1.pdf'))
@@ -12,32 +13,32 @@ file_dir2 = os.path.normpath(os.path.join(base_path, '../PDFtoBoW/sample2.pdf'))
 # 処理時間を計測
 s1 = time()
 # sample1.pdfの単語頻度、単語の意味のdictをそれぞれ取得
-sample1_frequency, sample1_meaning = get_BoW(file_dir1)
+sample1_frequency = get_BoW(file_dir1)
+sample1_meaning = get_meaning(file_dir1)
 g1 = time()
-print('Test result on sample1:')
-print(f'Processing time: {g1-s1:.4f} [s]')
+print(f'Processing time on {file_dir1}: {g1-s1:.4f} [s]')
 
 s2 = time()
 # sample2.pdfの単語頻度、単語の意味のdictをそれぞれ取得
-sample2_frequency, sample2_meaning = get_BoW(file_dir2)
+sample2_frequency = get_BoW(file_dir2)
+sample2_meaning = get_meaning(file_dir2)
 g2 = time()
-print('Test result on sample2:')
-print(f'Processing time: {g2-s2:.4f} [s]')
+print(f'Processing time on {file_dir2}: {g2-s2:.4f} [s]')
 
 # 取得したBoW(dict)の内容をチェックするためにjsonファイルとして出力
 # sample1.pdfの{'単語': 頻度}のdictを保存
-# with open('sample1_frequency.json', 'w') as f:
-#     json.dump(sample1_frequency, f, indent = 4)
+with open('sample1_frequency.json', 'w') as f:
+    json.dump(sample1_frequency, f, indent = 4)
 # sample2.pdfの{'単語': 頻度}のdictを保存
-# with open('sample2_frequency.json', 'w') as f:
-#     json.dump(sample2_frequency, f, indent = 4)
+with open('sample2_frequency.json', 'w') as f:
+    json.dump(sample2_frequency, f, indent = 4)
 
 # sample1.pdfの{'単語'; '辞書のエントリー'}のdictを保存
-# with open('sample1_meaning.json', 'w', encoding = 'utf-8') as f:
-#     json.dump(sample1_meaning, f, indent = 4)
+with open('sample1_meaning.json', 'w', encoding = 'utf-8') as f:
+    json.dump(sample1_meaning, f, indent = 4)
 # sample2.pdfの{'単語'; '辞書のエントリー'}のdictを保存
-# with open('sample2_meaning.json', 'w', encoding = 'utf-8') as f:
-#     json.dump(sample2_meaning, f, indent = 4)
+with open('sample2_meaning.json', 'w', encoding = 'utf-8') as f:
+    json.dump(sample2_meaning, f, indent = 4)
 
 # {'単語': '辞書のエントリー'}のBoWについてちゃんと対応できているかテスト
 print(sample1_meaning['activity'])
