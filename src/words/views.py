d2bcc48933702b7c703e350f0e7c2f052ac85dcb
makinzm <yuruseba@yahoo.co.jp>
2022-07-02 08:53:18 +0900

    readability

--- src/words/views.py
+++ src/words/views.py
@@ -107,16 +107,23 @@ class SubmitPDF(CreateView):
 
         def _calc_importance(i, i_order_scaled):
             # Impotanceを産出する(仮)
+            ratio_wiki = 0.7
+
+            ratio_bow = 1 - ratio_wiki
+
+            bonous_order = 20
+
             _v = int(100 * ((bow_dict[i] - min_bow_freq)/(max_bow_freq - min_bow_freq)))
-            _v = int(0.3*_v)
-            _importance = int(int(int(-wiki_dict[i] + 100)*(100/110))*0.7*0.3 + max(30, _v))
-            _importance = min(_importance + int(20*i_order_scaled), 100)
+            _v = int(ratio_bow*_v)
+            _importance = int( int(int(-wiki_dict[i] + 100)*(100/110))* ratio_wiki *0.3 \
+                + max(int(100*ratio_bow), _v))
+            _importance = min(_importance + int(bonous_order * i_order_scaled), 100)
             _importance = max(_importance, -10)
             return _importance
         
         for ii, (i,v) in enumerate(sorted(bow_dict.items(),key = lambda x: x[1])):
-            _importance = _calc_importance(i, ii/len(bow_dict))
-            print(ii, i, _importance, int(100 * ((bow_dict[i] - min_bow_freq)/(max_bow_freq - min_bow_freq))), bow_dict[i])
+            # _importance = _calc_importance(i, ii/len(bow_dict))
+            # print(ii, i, "importance_[-10,100]", _importance, "bow_[0,100]", int(100 * ((bow_dict[i] - min_bow_freq)/(max_bow_freq - min_bow_freq))),"bow_dict",bow_dict[i])
             if not Word.objects.filter(word=i).exists():
                 Word.objects.create(word=i,
                             importance = _calc_importance(i, ii/len(bow_dict)) , # 文章中の出現度とwikiによる値で調節.
